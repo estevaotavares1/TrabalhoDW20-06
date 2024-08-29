db22eadfffd8e370f18b39290937bce8f8e183f9
deborahnatalia74 <deborahnatalia74@gmail.com>
2024-08-29 16:46:58 -0300

    primeiras funçoes
    do cadastro de pessoas

--- codigo/cad_pessoa.php
+++ codigo/cad_pessoa.php
@@ -7,25 +7,9 @@ $cpf = $_POST['cpf'];
 
 require_once 'conexao.php';
 
-$sql = "INSERT INTO tb_cliente (nome, endereco, telefone) VALUES (?, ?, ?)";
+require_once "operacoes.php";
 
-$stmt = mysqli_prepare($conexao, $sql);
-
-mysqli_stmt_bind_param($stmt, "ssi", $nome, $endereco, $telefone);
-
-mysqli_stmt_execute($stmt);
-
-mysqli_stmt_close($stmt);
-////////////////////////////////////////////////////////////////////////////////////////////////
-$sql2 = "INSERT INTO tb_pessoafisica (cpf_pessoa) VALUES (?)";
-
-$stmt2 = mysqli_prepare($conexao, $sql2);
-
-mysqli_stmt_bind_param($stmt2, "i", $cpf_pessoa);
-
-mysqli_stmt_execute($stmt2);
-
-mysqli_stmt_close($stmt2);
+$resultados = cadastro_pessoa($conexao, $nome, $endereco, $telefone);
 
 header("Location: index.html");
 
---
+++ codigo/operacoes.php
@@ -0,0 +1,42 @@
+<?php
+
+function cadastro_pessoa($conexao, $nome, $endereco, $telefone)
+{
+    $sql = "INSERT INTO tb_cliente (nome, endereco, telefone) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($conexao, $sql);
+    mysqli_stmt_bind_param($stmt, "ssi", $nome, $endereco, $telefone);
+    mysqli_stmt_execute($stmt);
+
+    $sql2 = "INSERT INTO tb_pessoafisica (cpf_pessoa) VALUES (?)";
+    $stmt2 = mysqli_prepare($conexao, $sql2);
+    mysqli_stmt_bind_param($stmt2, "i", $cpf_pessoa);
+    mysqli_stmt_execute($stmt2);
+
+    $id = mysqli_stmt_insert_id($stmt);
+    mysqli_stmt_close($stmt);
+    mysqli_stmt_close($stmt2);
+
+    return $id;
+}
+
+function cadastro_empresa($conexao, $nome, $endereco, $telefone)
+{
+    $sql = "INSERT INTO tb_cliente (nome, endereco, telefone) VALUES (?, ?, ?)";
+    $stmt = mysqli_prepare($conexao, $sql);
+    mysqli_stmt_bind_param($stmt, "ssi", $nome, $endereco, $telefone);
+    mysqli_stmt_execute($stmt);
+
+    $sql2 = "INSERT INTO tb_empresa (cnpj_empresa) VALUES (?)";
+    $stmt2 = mysqli_prepare($conexao, $sql2);
+    mysqli_stmt_bind_param($stmt2, "i", $cnpj_empresa);
+    mysqli_stmt_execute($stmt2);
+
+    $id = mysqli_stmt_insert_id($stmt);
+    mysqli_stmt_close($stmt);
+    mysqli_stmt_close($stmt2);
+
+    return $id;
+}
+
+
+?>
