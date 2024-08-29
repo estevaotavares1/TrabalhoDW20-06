7054150faceab7ac3c24937dd668c6634d92edfd
estevaotavares1 <estevaot.d.a13@gmail.com>
2024-08-29 15:32:25 -0300

    deu tudo errado
    arrumando empresa e pessoa

--- codigo/cad_empresa.php
+++ codigo/cad_empresa.php
@@ -7,16 +7,3 @@ $cnpj = $_POST['cnpj'];
 
 require_once 'conexao.php';
 
-$sql = "INSERT INTO tb_empresa (nome, endereco, telefone, cnpj) VALUES (?, ?, ?, ?)";
-
-$stmt = mysqli_prepare($conexao, $sql);
-
-mysqli_stmt_bind_param($stmt, "ssii", $nome, $endereco, $telefone, $cnpj);
-
-mysqli_stmt_execute($stmt);
-
-mysqli_stmt_close($stmt);
-
-header("Location: index.html");
-
-?>
--- codigo/cad_pessoa.php
+++ codigo/cad_pessoa.php
@@ -7,15 +7,25 @@ $cpf = $_POST['cpf'];
 
 require_once 'conexao.php';
 
-$sql = "INSERT INTO tb_pessoafisica (nome, endereco, telefone, cpf) VALUES (?, ?, ?, ?)";
+$sql = "INSERT INTO tb_cliente (nome, endereco, telefone) VALUES (?, ?, ?)";
 
 $stmt = mysqli_prepare($conexao, $sql);
 
-mysqli_stmt_bind_param($stmt, "ssii", $nome, $endereco, $telefone, $cpf);
+mysqli_stmt_bind_param($stmt, "ssi", $nome, $endereco, $telefone);
 
 mysqli_stmt_execute($stmt);
 
 mysqli_stmt_close($stmt);
+////////////////////////////////////////////////////////////////////////////////////////////////
+$sql2 = "INSERT INTO tb_pessoafisica (cpf_pessoa) VALUES (?)";
+
+$stmt2 = mysqli_prepare($conexao, $sql2);
+
+mysqli_stmt_bind_param($stmt2, "i", $cpf_pessoa);
+
+mysqli_stmt_execute($stmt2);
+
+mysqli_stmt_close($stmt2);
 
 header("Location: index.html");
 
