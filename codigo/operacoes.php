8c3ffdac59a2d36b36a7697f046460dd4e697b49
AnnaPaulaa <silvaannapaula21@gmail.com>
2024-09-19 15:31:35 -0300

    mudanças
    Nas funçoes

--- codigo/operacoes.php
+++ codigo/operacoes.php
@@ -67,8 +67,8 @@ function salvarVeiculo($conexao, $nome, $marca, $ano, $tipo_veiculo, $placa_veic
 
     return $id;
 }
-function salvarEmprestimo($conexao, $id_funcionario, $id_cliente) {
-  $sql = "INSERT INTO tb_aluguel (id_funcionario,idcliente) VALUES (?, ?)";
+function salvarEmprestimo($conexao, $tb_funcionario_id_funcionario, $tb_cliente_id_cliente) {
+  $sql = "INSERT INTO tb_aluguel (tb_funcionario_id_funcionario,tb_cliente_id_cliente) VALUES (?, ?)";
   $stmt = mysqli_prepare($conexao, $sql);
 
   mysqli_stmt_bind_param($stmt, "ii", $tb_funcionario_id_funcionario,$tb_cliente_id_cliente);
