a865f5fbb41bbccd89f6855efda65425973d3ff0
daviapolin17 <130306075+daviapolin17@users.noreply.github.com>
2024-09-23 08:41:24 -0300

    Update formClientePagamento.php

--- codigo/formClientePagamento.php
+++ codigo/formClientePagamento.php
@@ -6,24 +6,33 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="formSelecionarEmprestimo.php">
+    <form action="formSelecionarEmprestimo.php" method="GET">
         Cliente: <br>
-        <select name="id_cliente">
+        <select name="id_cliente" required>
             <?php
                 require_once "conexao.php";
                 require_once "operacoes.php";
 
+                // Lista os clientes disponíveis
                 $clientes = listarClientes($conexao);
 
-                foreach ($clientes as $cliente) {
-                    $id_cliente = $cliente[0];
-                    $nome = $cliente[1];
-                    $endereco = $cliente[2];
-                    $telefone  = $cliente[3];
-                    echo "<option value='$id_cliente'>$nome</option>";
+                // Verifica se há clientes listados
+                if (!empty($clientes)) {
+                    foreach ($clientes as $cliente) {
+                        $id_cliente = $cliente[0] ?? ''; // Garantindo que a variável esteja definida
+                        $nome = $cliente[1] ?? '';
+                        $endereco = $cliente[2] ?? '';
+                        $telefone  = $cliente[3] ?? '';
+
+                        // Exibe as opções do dropdown
+                        echo "<option value='$id_cliente'>$nome</option>";
+                    }
+                } else {
+                    echo "<option disabled>Nenhum cliente disponível</option>";
                 }
             ?>
         </select>
+        <br><br>
         <input type="submit" value="Selecionar cliente">
     </form>
 </body>
