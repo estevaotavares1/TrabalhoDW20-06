eb8d200e19d39d4e42933b317d54c3edcd9fe8ef
daviapolin17 <130306075+daviapolin17@users.noreply.github.com>
2024-09-30 07:21:26 -0300

    Update formEmprestimo.php

--- codigo/formEmprestimo.php
+++ codigo/formEmprestimo.php
@@ -4,16 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulário de Empréstimo</title>
 </head>
 
 <body>
-    <form action="formSelecionarCarros.php">
-        <label for="datainicial_aluguel">Data Inicial do Émprestimo</label>
+    <form action="formSelecionarCarros.php" method="GET">
+        <label for="datainicial_aluguel">Data Inicial do Empréstimo</label>
         <input type="date" id="datainicial_aluguel" name="datainicial_aluguel" required><br><br>
 
-        <label for="datafinal_aluguel">Data Final do Émprestimo</label>
+        <label for="datafinal_aluguel">Data Final do Empréstimo</label>
         <input type="date" id="datafinal_aluguel" name="datafinal_aluguel" required><br><br>
+        
         Funcionário: <br>
         <select name="id_funcionario">
             <?php
@@ -23,26 +24,25 @@
             $funcionarios = listarFuncionarios($conexao);
 
             foreach ($funcionarios as $funcionario) {
-                $id_funcionario = $funcionario[0];
-                $nome_funcionario = $funcionario[1];
+                $id_funcionario = $funcionario['id_funcionario'];
+                $nome_funcionario = $funcionario['nome'];
                 echo "<option value='$id_funcionario'>$nome_funcionario</option>";
             }
             ?>
-        </select> <br><br>
+        </select><br><br>
+
         Cliente: <br>
         <select name="id_cliente">
             <?php
             $clientes = listarClientes($conexao);
 
             foreach ($clientes as $cliente) {
-                $id_cliente = $cliente[0];
-                $nome = $cliente[1];
-                $endereco = $cliente[2];
-                $telefone  = $cliente[3];
+                $id_cliente = $cliente['id_cliente'];
+                $nome = $cliente['nome'];
                 echo "<option value='$id_cliente'>$nome</option>";
             }
             ?>
-        </select> <br><br>
+        </select><br><br>
 
         <input type="submit" value="Selecionar carros">
     </form>
