19e4474fd4ea519cbab5e521761640e8664db635
deborahnatalia74 <159484284+deborahnatalia74@users.noreply.github.com>
2024-11-08 08:40:26 -0300

    Davi - Update cad_empresa.php

--- codigo/cad_empresa.php
+++ codigo/cad_empresa.php
@@ -1,8 +1,8 @@
 <?php
-$nome = $_POST['nome'] ?? '';
-$endereco = $_POST['endereco'] ?? '';
-$telefone = $_POST['telefone'] ?? '';
-$cnpj = $_POST['cnpj'] ?? '';
+$nome = $_POST['nome'];
+$endereco = $_POST['endereco'];
+$telefone = $_POST['telefone'];
+$cnpj = $_POST['cnpj'];
 
 require_once 'conexao.php';
 require_once "operacoes.php";
@@ -10,7 +10,7 @@ require_once "operacoes.php";
 if (!empty($nome) && !empty($endereco) && !empty($telefone) && !empty($cnpj)) {
     cadastro_empresa($conexao, $nome, $endereco, $telefone, $cnpj);
     header('Location: atividades.php');
-    exit; // Sempre bom adicionar um exit após header para evitar execução de código adicional
+    exit;
 } else {
     echo "Por favor, preencha todos os campos.";
 }
