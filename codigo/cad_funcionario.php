3fc109f1d285da418ce35070c6a0b199f8420c54
deborahnatalia74 <159484284+deborahnatalia74@users.noreply.github.com>
2024-11-08 08:40:52 -0300

    Davi - Update cad_funcionario.php

--- codigo/cad_funcionario.php
+++ codigo/cad_funcionario.php
@@ -1,9 +1,9 @@
 <?php
 
-$nome_funcionario = $_POST['nome_funcionario'] ?? '';
-$cpf_funcionario = $_POST['cpf_funcionario'] ?? '';
-$email_funcionario = $_POST['email_funcionario'] ?? '';
-$telefone_funcionario = $_POST['telefone_funcionario'] ?? '';
+$nome_funcionario = $_POST['nome_funcionario'];
+$cpf_funcionario = $_POST['cpf_funcionario'];
+$email_funcionario = $_POST['email_funcionario'];
+$telefone_funcionario = $_POST['telefone_funcionario'];
 
 require_once 'conexao.php';
 require_once "operacoes.php";
@@ -11,7 +11,7 @@ require_once "operacoes.php";
 if (!empty($nome_funcionario) && !empty($cpf_funcionario) && !empty($email_funcionario) && !empty($telefone_funcionario)) {
     salvarFuncionario($conexao, $nome_funcionario, $cpf_funcionario, $email_funcionario, $telefone_funcionario);
     header('Location: atividades.php');
-    exit; // Adiciona um exit para evitar execução de código adicional
+    exit;
 } else {
     echo "Por favor, preencha todos os campos.";
 }
