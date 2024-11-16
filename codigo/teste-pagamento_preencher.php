e4e9a37620877b371ef098dbfddcccbf07bb6558
daviapolin17 <davi.apolinario277@gmail.com>
2024-11-16 14:09:07 -0300

    .

--- codigo/teste-pagamento_preencher.php
+++ codigo/teste-pagamento_preencher.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lançar Pagamento</title>
+    <link rel="stylesheet" href="estilos/style.css" />
 </head>
 
 <body>
