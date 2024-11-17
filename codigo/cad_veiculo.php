8b6c9e875837a7747ccdf3ba69a4d5f7b83fd30f
daviapolin17 <davi.apolinario277@gmail.com>
2024-11-17 15:20:58 -0300

    CADASTRO CERTO
    Função do telefone removida por bobeira, mas vai ficar assim msm.
    
    Veículo agora pode cadastrar com capacidade e KM 0

--- codigo/cad_veiculo.php
+++ codigo/cad_veiculo.php
@@ -15,7 +15,7 @@ $km_veiculo = $_POST['km_veiculo'];
 require_once 'conexao.php';
 require_once "operacoes.php";
 
-if (!empty($nome) && !empty($marca) && !empty($ano) && !empty($placa_veiculo) && !empty($capacidade_veiculo) && !empty($km_veiculo)) {
+if (!empty($nome) && !empty($marca) && !empty($ano) && !empty($placa_veiculo)) {
     salvarVeiculo($conexao, $nome, $marca, $ano, $placa_veiculo, $capacidade_veiculo, $vidroeletrico_veiculo, $airbag_veiculo, $capacidaportamala_veiculo, $arcondicionado_veiculo, $automatico_veiculo, $km_veiculo);
     header('Location: atividades.php');
     exit;
--- codigo/listar_funcionarios.php
+++ codigo/listar_funcionarios.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Funcionários</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo/style.css">
 </head>
 
 <body>
