46724ff70e281f7f741d139826f8e32d27fe4736
Estevão Tavares <145158298+estevaotavares1@users.noreply.github.com>
2024-11-22 08:52:07 -0300

    Update listar_clientes.php

--- codigo/listar_clientes.php
+++ codigo/listar_clientes.php
@@ -157,8 +157,8 @@ require_once 'testalogin.php';
                             echo "<td>$telefone</td>";
                             echo "<td>$documento</td>";
                             echo "<td>
-                                    <a href=\"editar_cliente.php?tipo=$tipo&id_cliente=$id_cliente\" class=\"btn btn-warning btn-sm\">Editar</a>
-                                    <a id='excluir' href='excluir_cliente.php?id_cliente=$id_cliente' class='btn btn-danger btn-sm' onclick='return confirm(\"Tem certeza que deseja excluir?\");'>Excluir</a>
+                                    <a href=\"editarDefinitivo.php?tipo=$tipo&id_cliente=$id_cliente\" class=\"btn btn-warning btn-sm\">Editar</a>
+                                    <a id='excluir' href='#' class='btn btn-danger btn-sm' onclick='return confirm(\"Tem certeza que deseja excluir?\");'>Excluir</a>
                                   </td>";
                             echo "</tr>";
                         }
