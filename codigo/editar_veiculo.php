24366871cb806c270dd6346c24c11f4c5a7d182a
daviapolin17 <davi.apolinario277@gmail.com>
2024-11-25 20:29:10 -0300

    Pode editar
    Bloqueio da edição é por status

--- codigo/editar_veiculo.php
+++ codigo/editar_veiculo.php
@@ -5,27 +5,34 @@ require_once "conexao.php";
 if (isset($_GET['id_veiculo'])) {
     $id_veiculo = $_GET['id_veiculo'];
 
-    $sql_verificar_aluguel = "SELECT * FROM tb_aluguel_has_tb_veiculo WHERE tb_veiculo_id_veiculo = $id_veiculo";
-    $resultado_aluguel = mysqli_query($conexao, $sql_verificar_aluguel);
+    $sql_verificar_status = "SELECT status FROM tb_veiculo WHERE id_veiculo = $id_veiculo";
+    $resultado_status = mysqli_query($conexao, $sql_verificar_status);
 
-    if (mysqli_num_rows($resultado_aluguel) > 0) {
-        header('Location: erro2.php');
-        exit;
+    if (mysqli_num_rows($resultado_status) > 0) {
+        $linha = mysqli_fetch_array($resultado_status);
+        $status_veiculo = $linha['status'];
+
+        if ($status_veiculo == 'Indisponível') {
+            header('Location: erro2.php');
+            exit;
+        } else {
+            $sql = "SELECT * FROM tb_veiculo WHERE id_veiculo = $id_veiculo";
+            $resultado = mysqli_query($conexao, $sql);
+            $linha = mysqli_fetch_array($resultado);
+            $nome = $linha['nome'];
+            $marca = $linha['marca'];
+            $ano = $linha['ano'];
+            $placa_veiculo = $linha['placa_veiculo'];
+            $capacidade_veiculo = $linha['capacidade_veiculo'];
+            $vidroeletrico_veiculo = $linha['vidroeletrico_veiculo'];
+            $airbag_veiculo = $linha['airbag_veiculo'];
+            $capacidaportamala_veiculo = $linha['capacidaportamala_veiculo'];
+            $arcondicionado_veiculo = $linha['arcondicionado_veiculo'];
+            $automatico_veiculo = $linha['automatico_veiculo'];
+            $km_veiculo = $linha['km_veiculo'];
+        }
     } else {
-        $sql = "SELECT * FROM tb_veiculo WHERE id_veiculo = $id_veiculo";
-        $resultado = mysqli_query($conexao, $sql);
-        $linha = mysqli_fetch_array($resultado);
-        $nome = $linha['nome'];
-        $marca = $linha['marca'];
-        $ano = $linha['ano'];
-        $placa_veiculo = $linha['placa_veiculo'];
-        $capacidade_veiculo = $linha['capacidade_veiculo'];
-        $vidroeletrico_veiculo = $linha['vidroeletrico_veiculo'];
-        $airbag_veiculo = $linha['airbag_veiculo'];
-        $capacidaportamala_veiculo = $linha['capacidaportamala_veiculo'];
-        $arcondicionado_veiculo = $linha['arcondicionado_veiculo'];
-        $automatico_veiculo = $linha['automatico_veiculo'];
-        $km_veiculo = $linha['km_veiculo'];
+        echo "Veículo não encontrado";
     }
 } else {
     $id_veiculo = 0;
